c97b6c914b9089670e28c3b456dba1170f3aeff2
Andredangad <47104331+Andredangad@users.noreply.github.com>
2020-05-17 00:44:47 +0200

    exo2

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 node *read_tree(FILE *infile) {
     char c;
@@ -16,13 +17,29 @@ node *read_tree(FILE *infile) {
       n->right = read_tree(infile);
     }
     return n;
-    
+}
+
+void init_array(int array[], int size){
+    int i;
+    for(i =0; i<size;i++)
+        array[i] = 0;
+}
+void create_frequency_array(FILE *infile, int frequency[]){
+    char c;
+    while((c = getc(infile)) != EOF)
+        frequency[(unsigned char) c] = frequency[(unsigned char) c] +1;
+}
+
+void encode_file(char *code_table[], FILE *infile, FILE *outfile){
+    char c;
+    while((c = getc(infile)) != EOF)
+        fprintf(outfile, "%s ", code_table[(unsigned char) c]);
 }
 
 int main(int argc, char **argv) {
     
-    if (argc < 2) {
-        fprintf(stderr, "Usage: concordance <in_file>\n");
+    if (argc < 3) {
+        fprintf(stderr, "Usage: concordance <in_file> <out_file>\n");
         return 1;
     }
 
@@ -31,14 +48,47 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Error opening file for reading: %s\n", argv[1]);
         return 1;
     }
+
+    FILE *fout = fopen(argv[2], "w");
+    if (fout == NULL) {
+        fprintf(stderr, "Error opening file for writing: %s\n", argv[2]);
+        return 1;
+    }
+
+    int frequency[256];
+    init_array(frequency, 256);
+    create_frequency_array(fin, frequency);
+    int i;
+    for(i =0; i<256;i++)
+        printf("%d : %d\n",i, frequency[i]);
+    /*
+    node *n = NULL;
+    n = read_tree(fin);
+
+    write_tree(n);
+
+    int pos[100]; 
+    tab(n,0, pos);
+    */
+ /*    char *code_table[256];
+    code_table[97] = "0";
+    code_table[98] = "10";
+    code_table[99] = "1100";
+    code_table[100] = "1101";
+    code_table[114] = "111"; */
+
     node *n = NULL;
     n = read_tree(fin);
     write_tree(n);
     char pos[100]; 
     char *code_table[256];
     tab(n,0, pos, code_table);
-    printf("%s\n", code_table[100]);
-    printf("%s\n", code_table[114]);
+
+   
+    rewind(fin);
+    encode_file(code_table, fin, fout);
+
     fclose(fin);
+    fclose(fout);
     return 0;
 }
