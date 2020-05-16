a96405b1e33ff9440b15fe7ad52cf0bf319b7366
SkyySan <simassie@orange.fr>
2020-05-16 22:46:55 +0200

    Update tree.c

--- tree.c
+++ tree.c
@@ -13,15 +13,12 @@ node *create_node(char data) {
   return n;
 }
 
-
-
 void display_prefix(node *t){
   if(t==NULL)
     return;
   printf("%c\n",t->data);
   display_prefix(t->left);
   display_prefix(t->right);
-
 }
 
 int is_int_node(node* t){
@@ -29,26 +26,21 @@ int is_int_node(node* t){
     return 0;
   else
     return 1;
-
 }
 
 void printTab(int arr[], int n) { 
     int i; 
     for (i = 0; i < n; ++i) 
         printf("%d", arr[i]); 
-  
     printf("\n"); 
 } 
 
 void tab(node *t, int i, int pos[]){
   node *p;
 
- 
   if(t==NULL)
     return;
   if(is_int_node(t)){
-      
-     
       if(t->left){
         pos[i] = 0;
         tab(t->left, i+1, pos);
@@ -56,18 +48,12 @@ void tab(node *t, int i, int pos[]){
       if(t->right){
         pos[i] = 1;
         tab(t->right, i+1, pos);
-        
       }
-
   }
   else{
-
     printf("%c = ", t->data);
     printTab(pos,i);
-
   }
-
-
 }
 
 void free_tree(node *t){
